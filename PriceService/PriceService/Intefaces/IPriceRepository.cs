6ecb35c2dacdaedd6f9a8ab68af76da285a93f2f
senka.cicarevic <senka.cicarevic@gmail.com>
2021-08-03 20:44:42 +0200

    Price service

---
+++ PriceService/PriceService/Intefaces/IPriceRepository.cs
@@ -0,0 +1,15 @@
+﻿using PriceService.DTOs;
+using System;
+using System.Collections.Generic;
+
+namespace PriceService.Intefaces
+{
+    public interface IPriceRepository
+    {
+        List<PriceReadDTO> Get();
+        PriceReadDTO Get(Guid id);
+        PriceConfirmationDTO Create(PriceCreateDTO dto);
+        PriceConfirmationDTO Update(Guid id, PriceCreateDTO dto);
+        void Delete(Guid id);
+    }
+}
