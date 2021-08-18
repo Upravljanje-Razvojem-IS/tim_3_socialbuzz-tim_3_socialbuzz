69294e4929ab1fb2299708140ab641912226c124
ZuvelaTamara <zuvelatamara@gmail.com>
2021-08-18 16:58:48 +0200

    Added some comments and adapted logic

--- DeliveryService/DeliveryService.API/BusinessLogic/PriceCalculation.cs
+++ DeliveryService/DeliveryService.API/BusinessLogic/PriceCalculation.cs
@@ -4,6 +4,12 @@
     {
         public static double Calculate(int pieces, double price, double priceDistance, double weightCoefficient)
         {
+            /*
+              Izračunava se ukupna cena kupovine. 
+              Prvi deo predstavlja ukupnu cenu dostave, dok drugi predstavlja ukupnu cenu proizvoda.
+              Ukupna cena dostave => cena razdaljine x koeficijent težine
+              Ukupna cena proizvoda => cena proizvoda x broj komada
+            */
             return (priceDistance * weightCoefficient) + (price * pieces);
         }
     }
