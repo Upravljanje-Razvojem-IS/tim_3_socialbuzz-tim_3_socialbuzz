ab309a32de1c238ccc63bc2e4fa40e48ac1a3808
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2021-09-11 23:19:58 +0200

    Added LogController with get and save methods, UserController with save and update methods

---
+++ LoggerService/LoggerService/Controllers/LoggerController.cs
@@ -0,0 +1,90 @@
+﻿using LoggerService.Contracts;
+using LoggerService.Dtos;
+using LoggerService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace LoggerService.Controllers
+{
+    [Route("api/logs")]
+    [ApiController]
+    public class LoggerController : ControllerBase
+    {
+        private readonly ILogService _logService;
+
+        public LoggerController(ILogService logService)
+        {
+            _logService = logService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetLogs([FromQuery] LogParamsDto logParams)
+        {
+            try
+            {
+                return Ok(new SuccessResponse<IList<LogDto>>
+                {
+                    Data = _logService.GetLogs(logParams),
+                    Status = 200,
+                    Timestamp = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new ErrorResponse
+                    {
+                        Message = "Something went wrong. please try again later.",
+                        Status = 500,
+                        Timestamp = DateTime.Now
+                    });
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult SaveLog([FromBody] SaveLogDto logDto)
+        {
+            try
+            {
+                var res = _logService.SaveLog(logDto);
+
+                return Created($"api/logs/{res.Id}", new SuccessResponse<LogDto>
+                {
+                    Data = res,
+                    Status = 201,
+                    Timestamp = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                var res = e.Message.Equals($"User with id {logDto.UserId} doesn't exist") ?
+                 NotFound(new ErrorResponse
+                 {
+                     Message = e.Message,
+                     Timestamp = DateTime.Now,
+                     Status = 404
+                 }) :
+                StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new ErrorResponse
+                    {
+                        Message = "Something went wrong. please try again later.",
+                        Status = 500,
+                        Timestamp = DateTime.Now
+                    }
+                );
+
+                return res;
+            }
+        }
+    }
+}
---
+++ LoggerService/LoggerService/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using LoggerService.Contracts;
+using LoggerService.Dtos;
+using LoggerService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LoggerService.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult SaveUser([FromBody] SaveUserDto userDto)
+        {
+            try
+            {
+                var user = _userService.SaveUser(userDto);
+                return Created($"/api/users/{user.Id}", new SuccessResponse<UserDto>
+                {
+                    Data = user,
+                    Status = 201,
+                    Timestamp = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                var res = e.Message.Equals($"User with id: {userDto.Id} already exists") ?
+                BadRequest(new ErrorResponse
+                {
+                    Message = e.Message,
+                    Status = 400,
+                    Timestamp = DateTime.Now
+                }) :
+                StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new ErrorResponse
+                    {
+                        Message = "Something went wrong. please try again later.",
+                        Status = 500,
+                        Timestamp = DateTime.Now
+                    }
+                );
+                return res;
+            }
+        }
+
+        [HttpPut]
+        [Route("/api/users/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UpdateUser([FromRoute] Guid id, UpdateUserDto userDto)
+        {
+            try
+            {
+                return Ok(new SuccessResponse<UserDto>
+                {
+                    Data = _userService.UpdateUser(id, userDto),
+                    Status = 200,
+                    Timestamp = DateTime.Now
+                });
+            }
+            catch (Exception e)
+            {
+                var res = e.Message.Equals($"User with id: {id} doesn't exist") ?
+                NotFound(new ErrorResponse
+                {
+                    Message = e.Message,
+                    Status = 404,
+                    Timestamp = DateTime.Now
+                }) :
+                StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new ErrorResponse
+                    {
+                        Message = "Something went wrong. please try again later.",
+                        Status = 500,
+                        Timestamp = DateTime.Now
+                    }
+                );
+                return res;
+            }
+        }
+    }
+}
