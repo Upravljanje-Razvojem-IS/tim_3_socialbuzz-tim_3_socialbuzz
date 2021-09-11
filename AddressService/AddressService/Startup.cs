f483e0190c67073127bd2d3fe9a384038a1ef45b
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2021-09-11 23:31:34 +0200

    Basic project classes Startup for dependency injection ,main program class and settings files

---
+++ AddressService/AddressService/Startup.cs
@@ -0,0 +1,61 @@
+using AddressService.Helpers.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace AddressService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContexts(Configuration);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddSwagger();
+            services.AddCorsPolicies();
+            services.AddRepositories();
+            services.AddServices();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint("/swagger/AddressServiceOpeApiSpecification/swagger.json", "Address Service API");
+                setupAction.RoutePrefix = "";
+            });
+
+            app.UseRouting();
+            app.UseCors("AllowAllOrigins");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
