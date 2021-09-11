aa3c1e62396b2e94d53aed9519fb9a7efd5098d6
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2021-09-11 23:32:23 +0200

    Added models for address service consisting of city and country

---
+++ AddressService/AddressService/Models/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressService.Models
+{
+    public class City
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ZipCode { get; set; }
+        public Country Country { get; set; }
+    }
+}
