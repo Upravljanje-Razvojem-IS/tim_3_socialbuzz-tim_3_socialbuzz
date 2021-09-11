d50116fa9f328676201021296856e300fe86518e
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2021-09-11 23:15:10 +0200

    Mapping from dtos to domain objects and reverse using AutoMapper library

---
+++ LoggerService/LoggerService/Profiles/LogProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using LoggerService.Dtos;
+using LoggerService.Model;
+
+namespace LoggerService.Profiles
+{
+    public class LogProfiles : Profile
+    {
+        public LogProfiles()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+            CreateMap<UpdateUserDto, User>();
+            CreateMap<SaveUserDto, User>();
+            CreateMap<HttpRequest, HttpRequestDto>();
+            CreateMap<HttpRequestDto, HttpRequest>();
+            CreateMap<HttpResponse, HttpResponseDto>();
+            CreateMap<HttpResponseDto, HttpResponse>();
+            CreateMap<RequestError, RequestErrorDto>();
+            CreateMap<RequestErrorDto, RequestError>();
+            CreateMap<Log, LogDto>();
+            CreateMap<SaveLogDto, Log>();
+        }
+    }
+}
