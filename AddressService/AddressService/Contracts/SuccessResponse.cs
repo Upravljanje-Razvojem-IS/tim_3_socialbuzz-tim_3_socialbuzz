d2720502ae182e53d59d6270973c4a756962c53a
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2021-09-11 23:40:21 +0200

    Added shape of responses in service, for SuccesssResponse and ErrorResponse

---
+++ AddressService/AddressService/Contracts/SuccessResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AddressService.Contracts
+{
+    public class SuccessResponse<T>
+    {
+        public int Status { get; set; }
+        public T Data { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
