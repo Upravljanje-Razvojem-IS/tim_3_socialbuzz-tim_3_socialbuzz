e41de5e956b54c84cbfe3d5d3c85299f2c829f41
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2021-09-11 23:18:17 +0200

    Added data shape for SuccessResponse and ErrorResponse

---
+++ LoggerService/LoggerService/Contracts/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LoggerService.Contracts
+{
+    public class ErrorResponse
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ LoggerService/LoggerService/Contracts/SuccessResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LoggerService.Contracts
+{
+    public class SuccessResponse<T>
+    {
+        public int Status { get; set; }
+        public T Data { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
